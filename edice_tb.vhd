0126fe2b44e6a294fcb1adaf3a590152410100f1
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2019-09-07 21:17:50 +0200

    Update edice_tb.vhd

--- edice_tb.vhd
+++ edice_tb.vhd
@@ -10,6 +10,7 @@ architecture tb of edice_tb is
     component edice
         port (  CLK, Cheat, rst, run : in std_logic;
                 R : in std_logic_vector(2 downto 0);
+                
                 dice_7 : out std_logic_vector(6 downto 0);
                 seg_7 : out std_logic_vector(6 downto 0)
             );
@@ -37,29 +38,40 @@ clk_process : process
         wait for clk_period/2;
 end process;
  
+ 
+stim_run: process
+    begin
+        Run <= '0';
+            wait for clk_period*5;
+        Run <= '1'; 
+            wait for clk_period*2;
+end process; 
+
+
+stim_cheat: process
+    begin
+        cheat <= '1';
+            wait for clk_period*50;
+        cheat <= '0'; 
+            wait for clk_period*50;
+end process; 
+ 
 -- Stimuli process       
 stim: process
        begin
-            run <='1';
-            cheat <= '0';
-            wait for clk_period*10;
-            r <= "000";
-            wait for clk_period*10;
-            r <= "001";
-            wait for clk_period*10;
-            cheat <= '1';
+            r <="001";
+            wait for clk_period*8;
             r <= "010";
-            wait for clk_period*10;
+            wait for clk_period*8;
             r <= "011";
-            run <= '0';
-            wait for clk_period*10;
+            wait for clk_period*8;
             r <= "100";
-            wait for clk_period*10;
+            wait for clk_period*8;
             r <= "101";
-            wait for clk_period*10;
+            wait for clk_period*8;
             r <= "110";
-            wait for clk_period*10;
+            wait for clk_period*8;
             r <= "111";
-            wait for clk_period*10;
+            wait for clk_period*8;
     end process;
 end;
