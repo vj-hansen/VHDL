1e1f5a8ba486953abdc0a1b9fdf18ea39ab5f562
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2019-09-12 13:13:20 +0200

    Update top.vhd

--- top.vhd
+++ top.vhd
@@ -6,18 +6,16 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity top is
   Port (clk, cheat, reset, run : in std_logic;
         R_in : in std_logic_vector(2 downto 0);
---        R_out : out std_logic_vector(2 downto 0);
         an : out std_logic_vector(3 downto 0);
         seg7 : out STD_LOGIC_VECTOR(6 downto 0);
         leds : out STD_LOGIC_VECTOR(6 downto 0));
 end top;
 
 architecture Behavioral of top is
-
---signal clear : std_logic;
 signal Res : std_logic_vector(2 downto 0);
 
 begin
+  
 counter : entity work.UniBitCnt port map (
             clk => clk,
             run => run,
