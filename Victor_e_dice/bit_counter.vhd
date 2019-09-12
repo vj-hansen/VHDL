e3bf70063415446373d88cb12768cfc21a21fefa
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2019-09-12 14:10:56 +0200

    Update bit_counter.vhd

--- Victor_e_dice/bit_counter.vhd
+++ Victor_e_dice/bit_counter.vhd
@@ -5,13 +5,13 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.std_logic_unsigned.all;
 use IEEE.numeric_std.ALL;
 
-entity UniBitCnt is
+entity counter_logic is
   Port (clk, run, reset, cheat : in std_logic;
         R_in : in std_logic_vector(2 downto 0);
         R_out : out std_logic_vector(2 downto 0));
-end UniBitCnt;
+end counter_logic;
 
-architecture Behavioral of UniBitCnt is
+architecture Behavioral of counter_logic is
     signal ffin, ffout : unsigned(2 downto 0);
     signal clear : std_logic;
 begin
@@ -27,16 +27,16 @@ begin
 end process;
 
 -- Next-state logic (combinational)
-ffin <= (others => '0') when (clear = '1') else 
-        ffout when (run = '0') else  -- no action (run = 0)
-        ffout + 1;
+ffin <= (others=>'0') when (clear='1') else 
+        ffout when (run='0') else  -- no action
+        ffout+1;
 
-clear <= ( (run) and (not cheat) and (ffout(2)) and (not ffout(1)) and (ffout(0)) )
-        OR ( (run) and (cheat) and (not R_in(2)) and (not R_in(1)) and (not R_in(0)) )  -- R_in = "000"
-        OR ( (run) and (cheat) and (R_in(2)) and (R_in(1)) and (R_in(0)) );             -- R_in = "111"
+clear <= '1' when (run='1' and cheat='0' and ffout="101") else
+         '1' when (run='1' and cheat='1' and R_in="000" and ffout="101") else
+         '1' when (run='1' and cheat='1' and R_in="111" and ffout="101");
 
 -- output logic (combinational)
-R_out <= std_logic_vector(ffout) when ffout <= 5 else
-         std_logic_vector(unsigned(R_in)-1) ;
+R_out <= std_logic_vector(ffout) when ffout<="101" else
+         std_logic_vector(unsigned(R_in)-1);
              
 end Behavioral;
