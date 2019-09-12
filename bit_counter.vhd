81d9ae0eb823cab0251fb971538c0056a9ff5c0b
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2019-09-12 11:32:23 +0200

    Update bit_counter.vhd

--- bit_counter.vhd
+++ bit_counter.vhd
@@ -1,4 +1,4 @@
--- 3-bit counter
+-- 3-bit counter (result generator)
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -24,10 +24,23 @@ begin
     end if;
 end process;
 
+      
+      clear <= ((run and not cheat) ffout(2) and not (ffout(1)) and ffout(0)))
+      or (run and cheat and not (rin(2)) and not (rin(1)) and not (rin(0))) -- R = 000
+      or (run and cheat and (rin(2)) and (rin(1)) and (rin(0))) -- R = 111
+      
+      
+      
 -- Next-state logic (combinational)
 ffin <= (others => '0') when (clear = '1') else
         ffout when (en = '0') else  -- no action (run = 0)
         ffout+1;   -- count up
+
+  
+  ---- sender ut tall fra counter om det er mindre enn 5 (=6 på terning),
+  ---- men 
+  rout <= std_logic_vector(ffout) when ffout <= 5 else
+    std_logic_vector(unsigned(rin)-1) -- send verdi for cheat (ffout > 5 --> går til X1 og x2)
 
 c_out <= std_logic_vector(ffout);
 end Behavioral;
