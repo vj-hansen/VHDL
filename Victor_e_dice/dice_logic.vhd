6d2ffee606cdd042701c5d0febd7ab483f88153b
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2019-09-13 18:46:45 +0200

    Update and rename bit_counter.vhd to dice_logic.vhd

---
+++ Victor_e_dice/dice_logic.vhd
@@ -0,0 +1,41 @@
+-- e-dice: dice_logic
+-- Victor Johan Hansen
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.std_logic_unsigned.all;
+use IEEE.numeric_std.ALL;
+
+entity counter_logic is
+  Port (clk, run, reset, cheat : in std_logic;
+        set_val : in std_logic_vector(2 downto 0); -- set value
+        state : out std_logic_vector(2 downto 0)); -- state
+end counter_logic;
+
+architecture Behavioral of counter_logic is
+    signal ffin, ffout : unsigned(2 downto 0);
+    signal clear : std_logic;
+begin
+-- State register section
+process (clk, reset)
+begin
+    if (reset = '1') then
+        ffout <= (others => '0');
+    elsif rising_edge(clk) then
+        ffout <= ffin;
+    end if;
+end process;
+      
+clear <='1' when ((run='1') AND (cheat='0') AND (ffout="101")) else
+        '1' when ((run='1') AND (cheat='1') AND (set_val="000") AND (ffout="101")) else
+        '1' when ((run='1') AND (cheat='1') AND (set_val="111") AND (ffout="101")) else
+        '0';
+          
+-- Next-state logic
+ffin <= (others=>'0') when (clear='1') else 
+        ffout when (run='0') else  -- no action
+        ffout+1;
+-- output logic
+state <= std_logic_vector(ffout) when ffout<="101" else
+         std_logic_vector(unsigned(set_val)-1);             
+end Behavioral;
